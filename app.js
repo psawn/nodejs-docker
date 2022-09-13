9899ff9f522a08972161050f24ec334bef4d5c1b
unknown <bao.doan@savvycomsoftware.com>
2022-09-13 16:18:24 +0000

    add docker compose and document

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "dbuser",
+  host: "db", // 172.17.0.2 || db || b6e8837d4eeb
+  database: "testdb",
+  password: "dbpass2022",
+  port: 5432,
+});
+
+// host is IP address, service name when link, container id
+// not working with container name
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
+});
+
+app.get("/user", function (req, res) {
+  pool.query("SELECT * FROM accounts", (error, results) => {
+    if (error) {
+      res.status(500).json(error);
+    }
+    res.status(200).json(results);
+  });
 });
 
 const port = process.env.PORT || 3000;
